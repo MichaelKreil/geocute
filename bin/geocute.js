83ea195a1f9f0b824dfaec2b11b5257d1e9ace79
yetzt <git@yetzt.me>
2022-09-16 20:23:34 +0200

    eyecandy

--- bin/geocute.js
+++ bin/geocute.js
@@ -4,36 +4,38 @@
 
 const fs = require('fs');
 const ffd = require('ffd');
-const colors = require('colors');
 const path = require('path');
+const colors = require('colrz');
 const ProgressBar = require('progress');
 const PointList = require('../lib/point_list.js');
 const RegionLookup = require('../lib/region_lookup.js');
+
+const print = require('../lib/print')("[geocute]".magenta);
 
 const minResidents = 10;
 
 const args = process.argv.slice(2);
 
-if ((args.length < 5) || (args.length > 6)) {
-	console.error('Wrong number of arguments! I need 5-6'.yellow);
-	console.error('Usage:');
-	console.error('  geocute geo1 key1 geo2 key2 [pointlist] output');
-	console.error('    - geo1: filename of source GeoJSON');
-	console.error('    - key1: property name of the key in source GeoJSON');
-	console.error('    - geo2: filename of target GeoJSON');
-	console.error('    - key2: property name of the key in target GeoJSON');
-	console.error('    - pointlist: (optional) name of list of points to through at the data. Can be:');
-	console.error('      - "../data/deutschland.bin.br": (default) based on telephone book entries');
-	console.error('      - "../data/berlin_blk.bin.br": Berlin only, based on "statistische Blöcke"');
-	console.error('      - "../data/berlin_adr_ew.bin.br": Berlin only, based on "Sonderauswertung RBS-Adressen"');
-	console.error('    - output: filename of resulting TSV file');
-	console.error('Examples:');
-	console.error('  If you want to calculate a matrix for converting from "gemeinden" to "wahlkreise", type:');
-	console.error('     node geocute gemeinden.geojson AGS wahlkreise.geojson wkr_nr matrix.tsv');
-	console.error('  If you want to convert from "wahlbezirk" to "plz" using "Sonderauswertung RBS-Adressen", type:');
-	console.error('     node geocute wahlbezirk.geojson WBZ plz.geojson plz ../data/berlin_adr.bin.br matrix.tsv');
-	process.exit();
-}
+if ((args.length < 5) || (args.length > 6) || args.indexOf("-h")>= 0 || args.indexOf("--help")>= 0) {
+	print('Wrong number of arguments! I need 5-6'.red);
+	print('Usage:');
+	print('  geocute geo1 key1 geo2 key2 [pointlist] output');
+	print('    - geo1: filename of source GeoJSON');
+	print('    - key1: property name of the key in source GeoJSON');
+	print('    - geo2: filename of target GeoJSON');
+	print('    - key2: property name of the key in target GeoJSON');
+	print('    - pointlist: (optional) name of list of points to through at the data. Can be:');
+	print('      - "../data/deutschland.bin.br": (default) based on telephone book entries');
+	print('      - "../data/berlin_blk.bin.br": Berlin only, based on "statistische Blöcke"');
+	print('      - "../data/berlin_adr_ew.bin.br": Berlin only, based on "Sonderauswertung RBS-Adressen"');
+	print('    - output: filename of resulting TSV file');
+	print('Examples:');
+	print('  If you want to calculate a matrix for converting from "gemeinden" to "wahlkreise", type:');
+	print('     node geocute gemeinden.geojson AGS wahlkreise.geojson wkr_nr matrix.tsv');
+	print('  If you want to convert from "wahlbezirk" to "plz" using "Sonderauswertung RBS-Adressen", type:');
+	print('     node geocute wahlbezirk.geojson WBZ plz.geojson plz ../data/berlin_adr.bin.br matrix.tsv');
+	process.exit(1);
+};
 
 const filename1   = args.shift();
 const key1        = args.shift();
@@ -43,29 +45,33 @@ const key2        = args.shift();
 const pointListFilenameArg = (args.length > 1) ? args.shift() : '../data/deutschland.bin.br'
 const filenameOut = args.shift();
 
-	if (pointListFilename === null) return console.error("Unable to find PointList '%s'", pointListFilename);
 ffd(pointListFilenameArg, [ process.cwd(), __dirname, path.resolve(__dirname,"../data") ], function(pointListFilename){
+	if (pointListFilename === null) return print("Unable to find PointList '%s'", pointListFilename);
 
-	console.log('load points');
+	print('load points from '+(path.basename(pointListFilename).gray));
 	const points = PointList.load(pointListFilename);
 
-	console.log('load regions "'+filename1+'"');
+	print('load regions from '+(filename1.gray)+'');
 	const geo1 = new RegionLookup(filename1);
 
-	console.log('load regions "'+filename2+'"');
+	print('load regions from '+(filename2.gray)+'');
 	const geo2 = new RegionLookup(filename2);
 
-	console.log('generate lookups "'+filename1+'"');
+	print('generate lookups from '+(filename1.gray)+'');
 	const lookup1 = geo1.getLookup(true);
 
-	console.log('generate lookups "'+filename2+'"');
+	print('generate lookups from '+(filename2.gray)+'');
 	const lookup2 = geo2.getLookup(true);
 
 	let sum = 0;
 	let misses = [];
 	let hits = new Map();
 	const count = points.getLength();
-	const bar = new ProgressBar('fire points [:bar] :percent (ETA :etas)', { total:50 });
+	const bar = new ProgressBar(('[geocute]'.magenta)+' fire points :bar :percent (ETA :etas)', { 
+		total: (process.env.COLUMNS || process.stdout.columns || 80),
+		complete: '▬'.magenta.bold,
+		incomplete: '▬'.black.bold,
+	});
 
 	points.forEach((p,i) => {
 		if (i % 100000 === 0) bar.update(i/count);
@@ -88,9 +94,7 @@ ffd(pointListFilenameArg, [ process.cwd(), __dirname, path.resolve(__dirname,"..
 	}, () => {
 		bar.update(1);
 
-		console.log('');
-
-		console.log('analyse results');
+		print('analyse results');
 	
 		hits = Array.from(hits.values());
 		hits.forEach(hit => {
@@ -127,59 +131,68 @@ ffd(pointListFilenameArg, [ process.cwd(), __dirname, path.resolve(__dirname,"..
 			};
 		});
 
-		console.log('- misses:');
-		console.log('   - in geo 1: '     +missSum1 +' ('+(100*missSum1 /sum).toFixed(3)+'%)');
-		console.log('   - in geo 2: '     +missSum2 +' ('+(100*missSum2 /sum).toFixed(3)+'%)');
-		console.log('   - in geo 1 or 2: '+missSum12+' ('+(100*missSum12/sum).toFixed(3)+'%)');
+		if (misses.length === 0) {
+			print('no misses ✓'.green);
+		} else {
+			print('misses:'.yellow);
+			print('	in geo 1: '     +missSum1 +' ('+(100*missSum1 /sum).toFixed(3)+'%)');
+			print('	in geo 2: '     +missSum2 +' ('+(100*missSum2 /sum).toFixed(3)+'%)');
+			print('	in geo 1 or 2: '+missSum12+' ('+(100*missSum12/sum).toFixed(3)+'%)');
 
-		if (misses.length > 0) {
-			console.log('   - saving all misses as "_misses.geojson"'.yellow);
+			print('   - saving all misses as "_misses.geojson"'.yellow);
 			fs.writeFileSync('_misses.geojson', JSON.stringify({ type: 'FeatureCollection', features: misses }), 'utf8');
 		};
 
-		console.log('- regions without hits:');
 		const features1 = geo1.features.filter(f => (!f.properties._count));
 		const features2 = geo2.features.filter(f => (!f.properties._count));
-		console.log('   - in geo 1: '+features1.length);
 
-		if (features1.length) {
-			console.warn('Warning: Some regions in geo 1 where not hit:'.yellow);
-			console.warn(colors.yellow(features1.map(f => f.properties[key1]).join(',')));
-			console.warn('Solution: Estimate matrix entries based on overlapping areas.'.yellow);
-
-			const findOverlaps = geo2.getOverlapFinder();
-			const noOverlaps = features1.filter(f1 => {
-				const overlaps = findOverlaps(f1);
-				if (overlaps.length === 0) {
-					console.warn(('Error: Can not find overlaps for '+f1.properties[key1]+' in geo2').red);
-					return true;
-				};
-				overlaps.forEach(overlap => hits.push({
-					key1: f1.properties[key1],
-					key2: overlap.feature.properties[key2],
-					fraction: overlap.fraction,
-					residents: 0,
-					error: 1,
-					method: 'overlapping area',
-				}));
-			});
+		if (features1.length+features1.length === 0) {
+			print('no regions without hits ✓'.green);
+		} else {
+			print('found regions without hits:'.yellow);
 			
-			if (noOverlaps.length > 0) {
-				console.warn(('Saving non overlapping regions as _nooverlaps.geojson').red);
-				fs.writeFileSync('_nooverlaps.geojson', JSON.stringify({ type: 'FeatureCollection', features: noOverlaps }), 'utf8');
+			
+			if (features1.length) {
+				print('%s regions without hits in %s: %s'.yellow, features1.length.toString().bold, "geo1".grey, features1.map(f => f.properties[key1]).join(', ').brightYellow);
+				print('Solution: Estimate matrix entries based on overlapping areas.'.grey);
+
+				const findOverlaps = geo2.getOverlapFinder();
+				const noOverlaps = features1.filter(f1 => {
+					const overlaps = findOverlaps(f1);
+					if (overlaps.length === 0) {
+						print('Error: Unable to find overlaps for %s in geo2'.red, f1.properties[key1]);
+						return true;
+					};
+					overlaps.forEach(overlap => hits.push({
+						key1: f1.properties[key1],
+						key2: overlap.feature.properties[key2],
+						fraction: overlap.fraction,
+						residents: 0,
+						error: 1,
+						method: 'overlapping area',
+					}));
+				});
+			
+				if (noOverlaps.length > 0) {
+					print('Saving non overlapping regions as %s'.red, '_nooverlaps.geojson'.grey);
+					fs.writeFileSync('_nooverlaps.geojson', JSON.stringify({ type: 'FeatureCollection', features: noOverlaps }), 'utf8');
+				};
 			};
-		};
 
-		console.log('   - in geo 2: '+features2.length);
+			if (features2.length) {
+				print('%s regions without hits in %s'.yellow, features2.length.toString().bold, "geo2".grey);
+				print('Solution: None'.red);
+				print('Saving hitless regions as %s'.red, '_nohits.geojson'.grey);
+				fs.writeFileSync('_nohits.geojson', JSON.stringify({ type: 'FeatureCollection', features: features2 }), 'utf8');
+				
+				print('failed'.red.bold);
+				process.exit(1);
+				
+			};
+			
+		}
 
-		if (features2.length) {
-			console.log('ERROR: CAN\'T FIX IT, THAT THERE IS NO HITS IN GEO2!!'.red);
-			console.log('SOLUTION: PANIC!'.red);
-			console.log('   - saving that as "_nohits.geojson"');
-			fs.writeFileSync('_nohits.geojson', JSON.stringify({ type: 'FeatureCollection', features: features2 }), 'utf8');
-		};
-
-		console.log('save results');
+		print('save results'.green);
 		hits = hits.map(hit => [
 			hit.key1,
 			hit.key2,
@@ -192,7 +205,8 @@ ffd(pointListFilenameArg, [ process.cwd(), __dirname, path.resolve(__dirname,"..
 
 		fs.writeFileSync(filenameOut, hits.join('\n'), 'utf8');
 
-		console.log('');
+		print('done'.green.bold);
+
 	});
 
 });
